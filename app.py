269ae73b039e274a30c9365221db25726a4ee814
Vasiliy Maletin <maletinvasilii@mail.ru>
2023-11-10 23:23:12 +0800

    added login

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+import secrets
+from flask import Flask, request, render_template, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex()
 
 
 @app.route('/')
-def main():
-    context = {'title': 'Главная'}
-    return render_template('base.html', **context)
+def index():
+    if 'email' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/cloth/')
@@ -25,6 +29,22 @@ def shoes():
 def jackets():
     context = {'title': 'Куртки'}
     return render_template('jackets.html', **context)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['email'] = request.form.get('email')
+        session['username'] = request.form.get('username')
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
